abbd07ec2775697c6153f6089ca61dc2b4d5c034
harjashanpreet <159940055+harjashanpreet@users.noreply.github.com>
2024-04-10 18:45:24 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>&copy; 2024 Harjashanpreet | Student ID: 202108238</p>
+    <a href="admin/login.php" class="admin-login-button">Admin Panel Login</a>
+    <a href="editor/login.php" class="editor-login-button">editor Panel Login</a>
+</footer>
